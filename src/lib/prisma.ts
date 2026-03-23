333f959bdc2937439ed9679a8cf8c08bf5901958
netbrandingmcc-web <netbrandingmcc@gmail.com>
2026-03-23 15:25:17 +0530

    changes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -13,32 +13,4 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 export const prisma =
   globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-  // eslint-disable-next-line no-var
-  var prismaPool: Pool | undefined;
-}
-
-function getPool() {
-  if (global.prismaPool) return global.prismaPool;
-  const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) {
-    throw new Error("DATABASE_URL is not set");
-  }
-  global.prismaPool = new Pool({ connectionString });
-  return global.prismaPool;
-}
-
-export const prisma =
-  global.prisma ??
-  new PrismaClient({
-    adapter: new PrismaPg(getPool()),
-    log:
-      process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
-
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
