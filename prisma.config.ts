82179a749ac920a82ec3a0b29c455b7b4d8cdd32
netbrandingmcc-web <netbrandingmcc@gmail.com>
2026-03-23 15:35:24 +0530

    changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from 'prisma/config'
-import { Pool } from 'pg'
 
 export default defineConfig({
-  
   schema: './prisma/schema.prisma',
-  migrate: {
-    async adapter() {
-      const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-      })
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      return new PrismaPg(pool)
-    },
-  },
 })
