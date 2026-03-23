f5882a4673ec978e0c15588c649ef03262cb7fff
netbrandingmcc-web <netbrandingmcc@gmail.com>
2026-03-23 21:12:52 +0530

    pass

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     async adapter() {
       const { PrismaPg } = await import('@prisma/adapter-pg')
       const pool = new Pool({
-        connectionString: process.env.postgresql://postgres:[13pGMEddhaGpVIEQ]@db.rzevyaunijdxsiflncpd.supabase.co:5432/postgres,
+        connectionString: process.env.postgresql://postgres:13pGMEddhaGpVIEQ@db.rzevyaunijdxsiflncpd.supabase.co:5432/postgres,
       })
       return new PrismaPg(pool)
     },
