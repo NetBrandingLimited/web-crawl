e0bfa4a6dc6017bbc9af5a4fae27e81558ad3703
netbrandingmcc-web <netbrandingmcc@gmail.com>
2026-03-24 06:35:59 +0530

    chanes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextConfig from 'eslint-config-next/core-web-vitals.js';
-import nextTs from "eslint-config-next/typescript.js";
+import nextConfig from "eslint-config-next/core-web-vitals.js";
 
 const eslintConfig = defineConfig([
-  ...nextConfig,
-  ...nextTs,
+  nextConfig,
   globalIgnores([
     ".next/**",
     "out/**",
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'prisma/config'
-import { Pool } from 'pg'
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL!,
-  },
-  migrate: {
-    async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-      })
-      return new PrismaPg(pool)
-    },
   },
 })
